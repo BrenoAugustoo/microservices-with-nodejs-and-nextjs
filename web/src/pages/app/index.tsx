9d5598579434fd0b1b154d20e3a4e0921cae6063
Breno Augusto <brenoaugusto98@gmail.com>
2022-04-07 15:43:07 -0300

    criando a base da aplicação multiserviço com nextjs, nestjs, prsima, graphql, docker

---
+++ web/src/pages/app/index.tsx
@@ -0,0 +1,18 @@
+import { useUser, withPageAuthRequired } from "@auth0/nextjs-auth0"
+
+export default function Home () {
+  const { user } = useUser();
+
+  return (
+    <>
+      <h1>Hello world</h1>
+      <pre>
+        {JSON.stringify(user, null, 2)}
+      </pre>
+
+      <a href="/api/auth/logout">Logout</a>
+    </>
+  )
+ }
+
+ export const getServerSideProps = withPageAuthRequired();
